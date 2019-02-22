9051e01362fb1db8950e192ae39f529631b80958
TheAnilDeveloper <anilsri.04@gmail.com>
2019-02-22 08:27:57 +0530

    Update Program.cs

--- FizzBuzzLogic/Program.cs
+++ FizzBuzzLogic/Program.cs
@@ -10,6 +10,31 @@ namespace FizzBuzzLogic
     {
         static void Main(string[] args)
         {
+            for (int number = 1; number < 100; number++)
+
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+
+                    Console.WriteLine("FizzBuzz {0}",number);
+                }
+
+                else if (number % 3 == 0)
+                {
+
+                    Console.WriteLine("Fizz {0}", number);
+                }
+
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz {0}", number);
+                }
+
+                else
+                {
+
+                    Console.WriteLine("{0}",number);
+
+                }
         }
     }
 }
